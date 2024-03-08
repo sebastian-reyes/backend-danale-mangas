970bd165660eea71cacd1005a0080947f1350deb
sebastian-reyes <sebastianreyes2105@gmail.com>
2024-03-07 21:03:48 -0500

    iniciando repositorio

---
+++ src/main/java/com/example/danalemangasapi/DanaleMangasApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.danalemangasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanaleMangasApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DanaleMangasApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/danalemangasapi/DanaleMangasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.danalemangasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanaleMangasApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
